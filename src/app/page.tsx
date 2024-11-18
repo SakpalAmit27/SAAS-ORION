7a3198f79fcb5efb2d97a52fe3ce66ee14c820b9
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-18 12:36:51 +0530

    creating a custom maxwidthwrapper component

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+const page = () => {
+  return <>
+  
+  <section className="relative py-24 sm:py-32 bg-brand-25">
+    {/* creating a custom max with wrapper component to reuse later on*/}
+    <MaxWidthWrapper>
+
+    </MaxWidthWrapper>
+  </section>
+  <section></section>
+  <section></section>
+  <section></section>
+  
+  </>
+}
+
+export default page;
