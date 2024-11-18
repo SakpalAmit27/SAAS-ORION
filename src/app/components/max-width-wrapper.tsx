205dd03d1cfd35db4c96bc79fbc094c1d3a99ad9
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-18 19:35:59 +0530

    fixed the import causing error , and added the cn for coditional classnames styling

--- src/app/components/max-width-wrapper.tsx
+++ src/app/components/max-width-wrapper.tsx
@@ -1,5 +1,16 @@
-export const MaxWidthWrapper  = ({className,children} : MaxWidthWrapperProps) => {
-    return <div>
+import { cn } from "@/utils"
+import { ReactNode } from "react"
 
-    </div>
+interface MaxWidthWrapperProps{
+    className?:string
+    children:ReactNode
+
+}
+
+export const MaxWidthWrapper  = ({className,children} : MaxWidthWrapperProps) => {
+    return(
+        <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",className)}>
+        {children}
+        </div>
+    ) 
 }
