f5d0599e2000cda6040cc6a33b464ee18c926a5b
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-19 23:19:20 +0530

    creating navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+
+
+const navbar = () => {
+  return (
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white backdrop-blur-lg transition-all bg-white/80">
+
+    </nav>
+  )
+}
+
+export default navbar
