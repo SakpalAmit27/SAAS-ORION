11e0947067a362c16fec30d827fc01af88d4fe38
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-19 19:47:47 +0530

    added the shiny-button

--- src/app/components/shiny-button.tsx
+++ src/app/components/shiny-button.tsx
@@ -14,18 +14,21 @@ const ShinyButton = (
     href,
     ...props}:ShinyButtonProps) => {
     return(
-        <Link 
-        href={href ?? "#"}
-         className={cn("group-relative flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-brand-700 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-brand-700 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2 ",
-         
-         className)} {...props}>
-            <span className="relative z-10 flex items-center gap-2">
-                {children}
-                <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]"/>
-            </span>
+        <Link
+      href={href ?? "#"}
+      className={cn(
+        "group relative flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-brand-700 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-brand-700 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",
+        className
+      )}
+      {...props}
+    >
+      <span className="relative z-10 flex items-center gap-2">
+        {children}
+        <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]" />
+      </span>
 
-            
-         </Link>
+      <div className="ease-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[165px] w-8 rotate-[35deg] bg-white opacity-40 transition-all duration-600 group-hover:left-[120%]"/>
+    </Link>
     )
 }
 
