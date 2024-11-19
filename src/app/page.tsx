d295380290c6eee339a4e5fcceeabc2e7f6f9727
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-19 17:45:13 +0530

    added the checkmark icon along with changes in css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { MaxWidthWrapper } from "./components/max-width-wrapper";
 import Heading from "./components/heading";
+import {Check} from "lucide-react"
 
 const page = () => {
   return <>
@@ -16,10 +17,10 @@ const page = () => {
           </Heading>
         </div>
         <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">SaaSOrion is the easiest way to monitor your SaaS. Get instant notifications for{" "}<span className="font-semibold text-gray-700">sales, new users, or any other event</span> sent directly to your Discord.</p>
-        <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col sm:items-start">
-        {["Real-time Discord alerts for critical events","buy once, use forever","Track sales ,new users, or any other event"].map((item,index)=>(
+        <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start">
+        {["Real-time Discord alerts for critical events","buy once, use forever","Track sales ,new users, or any other event",].map((item,index)=>(
           <li key={index} className="flex gap-1.5 items-center text-left">
-            
+            <Check className="size-5 shrink-0"/>
             {item}</li>
         ))}
         </ul>
