b2b80dde987ca7dd8898e238c24c62a201e00f5c
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-19 17:36:18 +0530

    added an array of strings listed , and mapped it in li

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,17 @@ const page = () => {
           <Heading>
             <span>Real-Time SaaS Insights,</span>
             <br />
-            <span className="relative bg-gradient-to-r from-zinc-700 to-orange-300 text-transparent bg-clip-text ">Delivered to Your Discord</span>
+            <span className="relative bg-gradient-to-r from-zinc-700 to-blue-500 text-transparent bg-clip-text ">Delivered to Your Discord</span>
           </Heading>
         </div>
-        
+        <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">SaaSOrion is the easiest way to monitor your SaaS. Get instant notifications for{" "}<span className="font-semibold text-gray-700">sales, new users, or any other event</span> sent directly to your Discord.</p>
+        <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col sm:items-start">
+        {["Real-time Discord alerts for critical events","buy once, use forever","Track sales ,new users, or any other event"].map((item,index)=>(
+          <li key={index} className="flex gap-1.5 items-center text-left">
+            
+            {item}</li>
+        ))}
+        </ul>
       </div>
     </MaxWidthWrapper>
   </section>
