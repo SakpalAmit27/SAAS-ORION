4e7c26be1d82b7995dad19ab8f3d77be6c64d600
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-19 08:18:04 +0530

    added the childrens and headings and some headlines (temporary styles)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { MaxWidthWrapper } from "./components/max-width-wrapper";
+import Heading from "./components/heading";
 
 const page = () => {
   return <>
@@ -8,8 +9,13 @@ const page = () => {
     <MaxWidthWrapper className="text-center">
       <div className="relative mx-auto text-center flex flex-col items-center gap-10">
         <div>
-          <h1></h1>
+          <Heading>
+            <span>Real-Time SaaS Insights,</span>
+            <br />
+            <span className="relative bg-gradient-to-r from-zinc-700 to-orange-300 text-transparent bg-clip-text ">Delivered to Your Discord</span>
+          </Heading>
         </div>
+        
       </div>
     </MaxWidthWrapper>
   </section>
