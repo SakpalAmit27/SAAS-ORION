ded37cae309ace9bb379acff37256c87b6e875ff
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-20 13:47:46 +0530

    navchange

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white backdrop-blur-lg transition-all bg-white/80">
         <MaxWidthWrapper>
             <div className="flex h-16 items-center justify-between">
-                <Link href='/' className="flex z-40 font-logo text-5xl">orion</Link>
+                <Link href='/' className="flex z-40 font-logo text-4xl text-zinc-900 hover:text-red-500 ease-in-out">orion</Link>
             </div>
         </MaxWidthWrapper>
     </nav>
