fca0abfa6720d04df7fb609a78d131235118dbc4
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-01 18:52:46 +0530

    some changes in the shiny button

--- src/components/shiny-button.tsx
+++ src/components/shiny-button.tsx
@@ -3,33 +3,30 @@ import { ArrowRight } from "lucide-react"
 import Link from "next/link"
 import { AnchorHTMLAttributes } from "react"
 
-interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement>{
-    
-}
+interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
-const ShinyButton = (
-    
-    {children,
-    className,
-    href,
-    ...props}:ShinyButtonProps) => {
-    return(
-        <Link
+const ShinyButton = ({
+  className,
+  children,
+  href,
+  ...props
+}: ShinyButtonProps) => {
+  return (
+    <Link
       href={href ?? "#"}
       className={cn(
-        "group relative flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-zinc-800 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-zinc-700 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",
+        "group relative flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-zinc-900 px-8 text-base/9 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-zinc-900 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",
         className
       )}
       {...props}
     >
       <span className="relative z-10 flex items-center gap-2">
         {children}
-        <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]" />
+        <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[10px]" />
       </span>
 
-      <div className="ease-[cubic-bezier(0.29,1,0.30,2)] absolute -left-[75px] -top-[50px] -z-10 h-[165px] w-8 rotate-[35deg] bg-white opacity-40 transition-all duration-600 group-hover:left-[120%]"/>
+      <div className="ease-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-20 transition-all duration-500 group-hover:left-[120%]"/>
     </Link>
-    )
+  )
 }
-
-export default ShinyButton
+export default ShinyButton;
