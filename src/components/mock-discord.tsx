a68352d658e9b18a0c6611d1f543f143b7c545d6
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-01 18:55:39 +0530

    created the inner input mimicing the discord demo

--- src/components/mock-discord.tsx
+++ src/components/mock-discord.tsx
@@ -19,8 +19,18 @@ export const MockDiscordUI = ({children} : {children:ReactNode}) => {
                 <span className="text-lg font-semibold text-gray-400">{String.fromCharCode(65+i)}</span>
             </div>
         ))}
-            <div className=" group size-12 bg-discord-background rounded-3xl flex items-center justify-center mb-3 hover:rounded-xl transition-all duration-200 hover:bg-[#3ba55c] cursor-not-allowed">
+            <div className=" group size-12 mt-auto bg-discord-background rounded-3xl flex items-center justify-center mb-3 hover:rounded-xl transition-all duration-200 hover:bg-[#3ba55c] cursor-not-allowed">
                 <span className="text-lg font-semibold text-gray-400"><PlusCircle className="text-[#3ba55c] group-hover:text-white"/></span>
+            </div>
+        </div>
+
+        {/*dm list here . */}
+
+        <div className="hidden md:flex w-60 bg-[#2f3136] flex-col">
+            <div className="px-4 h-16 border-b border-[#202225] flex items-center shadow-sm">
+                <div className="w-full bg-[#202225] text-sm rounded px-2 h-8 items-center flex justify-center text-gray-500 cursor-not-allowed">
+                    Find or start a conversation
+                </div>
             </div>
         </div>
     </div>
